0537fb1d15ff17e9ffaa0b39994a585a3dd67e3a
ranajoy-cosmo <banerji@apc.in2p3.fr>
2015-10-06 15:42:54 +0200

    Beam class with arbitrary resolution

--- beam/beam_kernel.py
+++ beam/beam_kernel.py
@@ -3,61 +3,67 @@
 import numpy as np
 import healpy as hp
 import matplotlib.pyplot as plt
+import sys
 from simulation.lib.plotting.my_imshow import new_imshow
-from settings import settings
 
-def gaussian_2d(x, y):
-    factor = 2*np.sqrt(2*np.log(2))
-    sigma_major = settings.fwhm_major/factor
-    sigma_minor = settings.fwhm_minor/factor
-    x0, y0 = settings.center
-    theta = np.deg2rad(settings.tilt)
-    a = (np.cos(theta)**2)/(2*sigma_major**2) + (np.sin(theta)**2)/(2*sigma_minor**2)
-    b = -1*np.sin(2*theta)/(4*sigma_major**2) + np.sin(2*theta)/(4*sigma_minor**2)
-    c = (np.sin(theta)**2)/(2*sigma_major**2) + (np.cos(theta)**2)/(2*sigma_minor**2)
-    beam = np.exp(-1*(a*(x - x0)**2 + 2*b*(x - x0)*(y - y0) + c*(y - y0)**2)) 
-    if settings.normalise_beam:
-        norm = 2*np.pi*sigma_major*sigma_minor
-        return (1/norm)*beam
-    else:
-        return beam
+class Beam:
 
-def get_mesh():
-    factor = 2*np.sqrt(2*np.log(2))
-    fwhm_max = np.max((settings.fwhm_major, settings.fwhm_minor))
-    sigma = fwhm_max/factor
-    dd = hp.nside2resol(settings.nside, arcmin = True)
-    nxy = int(settings.beam_cutoff*sigma/dd)
-    x = np.linspace(-1.0*nxy*dd, nxy*dd, 2*nxy + 1)
-    y = np.linspace(-1.0*nxy*dd, nxy*dd, 2*nxy + 1)
-    print x.size, y.size
-    return np.meshgrid(x,y), y
+    def __init__(self, settings=None):
+        if settings is None:
+            print "Beam settings not loaded. Exiting"
+            sys.exit()
+    
+        self.settings = settings
 
-def display_beam_settings():
-    print "Major axis(FWHM) : ", settings.fwhm_major, "arcmins"
-    print "Minor axis(FWHM) : ", settings.fwhm_minor, "arcmins"
-    print "Center : ", settings.center
-    print "Tilt : ", settings.tilt, " degrees"
-    print "NSIDE : ", settings.nside
-    print "Pixel size : ", hp.nside2resol(settings.nside, arcmin = True)
-    print "No, of pixels per fwhm (minor-axis): ", 2*settings.fwhm_minor/hp.nside2resol(settings.nside, arcmin = True)
+    def gaussian_2d(self):
+        factor = 2*np.sqrt(2*np.log(2))
+        sigma_major = self.settings.fwhm_major/factor
+        sigma_minor = self.settings.fwhm_minor/factor
+        x0, y0 = self.settings.center
+        theta = np.deg2rad(settings.tilt)
+        x,y = self.mesh
+        a = (np.cos(theta)**2)/(2*sigma_major**2) + (np.sin(theta)**2)/(2*sigma_minor**2)
+        b = -1*np.sin(2*theta)/(4*sigma_major**2) + np.sin(2*theta)/(4*sigma_minor**2)
+        c = (np.sin(theta)**2)/(2*sigma_major**2) + (np.cos(theta)**2)/(2*sigma_minor**2)
+        self.kernel = np.exp(-1*(a*(x - x0)**2 + 2*b*(x - x0)*(y - y0) + c*(y - y0)**2)) 
+        if self.settings.normalise_beam:
+            norm = 2*np.pi*sigma_major*sigma_minor
+            self.kernel /= norm
 
-def plot_beam(beam):
-    fig, ax = plt.subplots()
-    im = new_imshow(ax, beam)
-    fig.colorbar(im, ax=ax)
-    plt.show()
+    def get_mesh(self):
+        factor = 2*np.sqrt(2*np.log(2))
+        sigma_max = max(self.settings.fwhm_major, self.settings.fwhm_minor)/factor
+        offset_max = max(np.abs(self.settings.center))
+        size = self.settings.beam_cutoff*sigma_max + offset_max
+        dd = self.settings.beam_resolution
+        nxy = int(size/dd)
+        x = np.linspace(-1.0*nxy*dd, nxy*dd, 2*nxy + 1)
+        y = np.linspace(-1.0*nxy*dd, nxy*dd, 2*nxy + 1)
+        self.del_beta = x 
+        self.mesh = np.meshgrid(x,y)
+
+    def display_beam_settings(self):
+        print "Major axis(FWHM) : ", self.settings.fwhm_major, "arcmins"
+        print "Minor axis(FWHM) : ", self.settings.fwhm_minor, "arcmins"
+        print "Center : ", self.settings.center
+        print "Tilt : ", self.settings.tilt, " degrees"
+        print "Pixel size : ", self.settings.beam_resolution, "arcmins" 
+        print "No, of pixels per fwhm (minor-axis): ", 2*self.settings.fwhm_minor/self.settings.beam_resolution
+
+    def plot_beam(self):
+        fig, ax = plt.subplots()
+        im = new_imshow(ax, self.kernel)
+        fig.colorbar(im, ax=ax)
+        plt.show()
 
 
 if __name__=="__main__":
-    (xx, yy), del_beta = get_mesh()
-    beam_kernel = gaussian_2d(xx, yy)
-    if settings.display_beam_settings:
-        display_beam_settings()
-    if settings.plot_beam:
-        plot_beam(beam_kernel)
+    from default_settings import settings
+    beam = Beam(settings)
+    beam.get_mesh()
+    beam.gaussian_2d()
 
-else:
-    (xx, yy), del_beta = get_mesh()
-    del_beta = np.deg2rad(del_beta/60.0)
-    beam_kernel = gaussian_2d(xx, yy)
+    if settings.display_beam_settings:
+        beam.display_beam_settings()
+    if settings.plot_beam:
+        beam.plot_beam()
--- beam/default_settings.py
+++ beam/default_settings.py
@@ -6,16 +6,16 @@ settings = generic_class.Generic()
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 # Resolution settings
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-settings.nside = 2048
-settings.beam_cutoff = 4
+settings.beam_resolution = 1.0              #arcmins
+settings.beam_cutoff = 4                    #sigmas
 
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 # Beam shape settings
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-settings.fwhm_major = 8.0
-settings.fwhm_minor = 8.0
-settings.center = (0.0, 0.0) 
-settings.tilt = 0.0
+settings.fwhm_major = 10.0                  #arcmins
+settings.fwhm_minor = 5.0                   #arcmins
+settings.center = (0.0, 5.0)                #arcmins
+settings.tilt = 45.0                        #degrees
 settings.normalise_beam = False
 
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
